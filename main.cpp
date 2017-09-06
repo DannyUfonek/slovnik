bf0a831f3067e7d337513a8ffea22bff0e9f37b0
DannyUfonek <DannyUfonek@seznam.cz>
2017-09-06 22:38:10 +0200

    intro commit
    created codeblocks project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*
+* SLOVNIK
+* èeský slovník implementovaný pomocí vyhledávacího stromu.
+* Každý uzel obsahuje písmeno a bool, zda-li cesta stromem konèící tímto písmenem je slovo.
+* Interakce s uživatelem probíhá pomocí konzole, slovník je schopný analyzovat text oddìlený mezerami a interpunkcí v souboru a najít chybnì napsaná slova.
+* Slovník výslednì chybnì napsaná slova buï pouze vypíše èi rovnou opraví v souboru, slovo po slovì.
+* Slovník je nutno nahrát do pamìti
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
